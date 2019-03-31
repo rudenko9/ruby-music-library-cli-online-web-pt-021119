a539f0864ace86edb3ead73acba0148820d73349
Nataliya R <n.rudenko@gmail.com>
2019-03-31 21:05:00 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -41,14 +41,14 @@ class Song
   song
   end 
   
-  def self.find_by_name(song_name)
+  def self.find_by_name(name)
     @@all.find do |song|
-      song.name == song_name
+      song.name == name
     end
   end 
     
-   def self.find_or_create_by_name(song_name)
-     self.find_by_name(song_name) || self.create(song_name) 
+   def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create(name) 
    end 
 
 def self.new_from_filename(file_name)
