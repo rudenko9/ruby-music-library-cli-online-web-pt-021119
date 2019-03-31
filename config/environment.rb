ac3009e7de3ed174c4d66975abfdcdf58f8c6d54
Nataliya R <n.rudenko@gmail.com>
2019-03-31 20:07:52 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,16 @@ module Concerns
   module Findable
     
     def self.find_by_name
-      self.all.
+      self.all.class.find do |song|
+        song.name == name
+      end 
     
   end 
-  
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name
+    end
+  end 
   
   
   end 
