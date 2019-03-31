58b448761b41db60ac857c162b658a7ac515b852
Nataliya R <n.rudenko@gmail.com>
2019-03-31 20:08:07 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,14 +9,7 @@ module Concerns
         song.name == name
       end 
   end 
-  def self.find_by_name(name)
-    @@all.find do |song|
-      song.name == name
-    end
-  end 
-  
-  
-  end 
+end 
 end
 
 require_all 'lib'
