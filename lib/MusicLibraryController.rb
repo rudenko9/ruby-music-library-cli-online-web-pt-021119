c8841814b661b2215af1cde460890e618de45eb9
Nataliya R <n.rudenko@gmail.com>
2019-03-31 21:38:27 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
   
   def initialize(path = "./db/mp3s")
-    importer = MusicIMporter.new(path)
+    importer = MusicImporter.new(path)
     importer.import
     
   end 
