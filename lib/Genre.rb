4f9beb4898571c2d03df43f0fc7010140cd06dc7
Nataliya R <n.rudenko@gmail.com>
2019-03-31 19:42:56 +0000

    Automatically backed up by Learn

---
+++ lib/Genre.rb
@@ -0,0 +1,33 @@
+class Genre 
+  attr_accessor :name, :songs
+   @@all = []
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all 
+    @@all = []
+  end 
+  
+  def save
+    @@all << self
+  end 
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre 
+  end 
+  
+  def artists
+    artists = @songs.collect do |song|
+      song.artist 
+      
+  end
+  artists.uniq
+end   
+end 
---
+++ lib/Song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist)
+    self.genre=(genre)
+    end
+    
+    def artist=(artist)
+      @artist = artist 
+      artist.add_song(self) unless artist.nil?
+    end 
+    
+  @@all = []
+  
+  def genre=(genre)
+    @genre = genre 
+    genre.songs << self unless genre.nil? || genre.songs.include?(self)
+    
+    
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+    @@all = []
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.create(name)
+  song = self.new(name)
+  song.save
+  song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name
+    end
+  end 
+    
+   def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create(name) 
+   end 
+
+end 
