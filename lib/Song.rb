6d65b6bed916c9148a7b55d803b82e83fc6db605
Nataliya R <n.rudenko@gmail.com>
2019-03-31 21:19:00 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -56,7 +56,7 @@ def self.new_from_filename(file_name)
     artist_name = name.split(" - ")[0]
     genre_name = name.split(" - ")[2].chomp(".mp3")
     song = self.find_or_create_by_name(song_name)
-    
+    song.artist = Artist.find_or_create_by_name(artist_name)
 end 
 
 def self.create_from_filename(name)
